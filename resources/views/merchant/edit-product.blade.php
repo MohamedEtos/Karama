c6da65ef916f6025162329b2aba44004fb5a196d
Mohamed Mahrous <69046042+MohamedEtos@users.noreply.github.com>
2023-12-08 01:07:05 +0200

    finsh edit product image

--- resources/views/merchant/edit-product.blade.php
+++ resources/views/merchant/edit-product.blade.php
@@ -4,36 +4,14 @@
 @endsection
 @section('page-header')
 				<!-- breadcrumb -->
-				<div class="breadcrumb-header justify-content-between">
-					<div class="my-auto">
+				<div class="breadcrumb-header justify-content-between mb-2 mt-2">
+					<div class="my-auto  ">
 						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">Pages</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ Empty</span>
+							<h4 class="content-title mb-0 my-auto ">تعديل المنتجات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ المنتج رقم {{$product->id}}</span>
 						</div>
 					</div>
-					<div class="d-flex my-xl-auto right-content">
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-info btn-icon ml-2"><i class="mdi mdi-filter-variant"></i></button>
-						</div>
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-danger btn-icon ml-2"><i class="mdi mdi-star"></i></button>
-						</div>
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-warning  btn-icon ml-2"><i class="mdi mdi-refresh"></i></button>
-						</div>
-						<div class="mb-3 mb-xl-0">
-							<div class="btn-group dropdown">
-								<button type="button" class="btn btn-primary">14 Aug 2019</button>
-								<button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" id="dropdownMenuDate" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<span class="sr-only">Toggle Dropdown</span>
-								</button>
-								<div class="dropdown-menu dropdown-menu-left" aria-labelledby="dropdownMenuDate" data-x-placement="bottom-end">
-									<a class="dropdown-item" href="#">2015</a>
-									<a class="dropdown-item" href="#">2016</a>
-									<a class="dropdown-item" href="#">2017</a>
-									<a class="dropdown-item" href="#">2018</a>
-								</div>
-							</div>
-						</div>
+
+
 					</div>
 				</div>
 				<!-- breadcrumb -->
@@ -109,8 +87,8 @@
 										<div class="col-md-4">
 											<div class="avatar-upload">
 												<div class="avatar-edit">
-													<input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" />
-													<label for="imageUpload"></label>
+													<input type='file' id="imageUpload1" accept=".png, .jpg, .jpeg" />
+													<label for="imageUpload1"></label>
 												</div>
 												<div class="avatar-preview">
 													<div id="imagePreview" style="background-image: url(http://i.pravatar.cc/500?img=7);">
@@ -121,11 +99,11 @@
 										<div class="col-md-4">
 											<div class="avatar-upload">
 												<div class="avatar-edit">
-													<input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" />
-													<label for="imageUpload"></label>
+													<input type='file' id="imageUpload2" accept=".png, .jpg, .jpeg" />
+													<label for="imageUpload2"></label>
 												</div>
 												<div class="avatar-preview">
-													<div id="imagePreview" style="background-image: url(http://i.pravatar.cc/500?img=7);">
+													<div id="imagePreview2" style="background-image: url(http://i.pravatar.cc/500?img=7);">
 													</div>
 												</div>
 											</div>
@@ -133,16 +111,22 @@
 										<div class="col-md-4">
 											<div class="avatar-upload">
 												<div class="avatar-edit">
-													<input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" />
-													<label for="imageUpload"></label>
+													<input type='file' id="imageUpload3" accept=".png, .jpg, .jpeg" />
+													<label for="imageUpload3"></label>
 												</div>
 												<div class="avatar-preview">
-													<div id="imagePreview" style="background-image: url(http://i.pravatar.cc/500?img=7);">
+													<div id="imagePreview3" style="background-image: url(http://i.pravatar.cc/500?img=7);">
 													</div>
 												</div>
 											</div>
 										</div>
 
+
+									</div>
+									<div class="row mt-5">
+										<div class="col-12">
+													<button class="btn btn-danger btn-block ">حفظ التعديلات</button>
+										</div>
 									</div>
 									<!-- row closed -->
 
