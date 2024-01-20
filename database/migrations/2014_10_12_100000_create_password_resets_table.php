ee75f1a7783df97d324be2498de018cecb314fe1
Mohamed Mahrous <69046042+MohamedEtos@users.noreply.github.com>
2024-01-20 23:08:22 +0200

    update reset password migration

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email',255)->primary();
+            // $table->string('email',255)->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
@@ -28,5 +28,6 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('password_resets');
+        
     }
 };
